f1aba22b42d97d913f4e14ee96f0840ef0da297b
Sundus Jawwad <sundus_bjw@hotmail.com>
2017-10-20 06:04:45 +0300

    kk

--- app/src/main/java/ps/wwbtraining/teacher_group1/TeacherApi.java
+++ app/src/main/java/ps/wwbtraining/teacher_group1/TeacherApi.java
@@ -5,7 +5,6 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.POST;
 
 /**
  * Created by Hanan Dawod on 18/10/17.
