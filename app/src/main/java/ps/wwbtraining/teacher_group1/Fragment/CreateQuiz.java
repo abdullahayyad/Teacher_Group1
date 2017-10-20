b8bf6e4b9a84ccb68bc9a6c320209fdaae167a2c
Sundus Jawwad <sundus_bjw@hotmail.com>
2017-10-20 06:00:53 +0300

    kk

--- app/src/main/java/ps/wwbtraining/teacher_group1/Fragment/CreateQuiz.java
+++ app/src/main/java/ps/wwbtraining/teacher_group1/Fragment/CreateQuiz.java
@@ -5,11 +5,36 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
 
 import ps.wwbtraining.teacher_group1.R;
 
 
 public class CreateQuiz extends Fragment {
+    private EditText tvnameQuize;
+    private EditText tvDiscription;
+    private EditText tvQuestion;
+    private RadioButton raObtion;
+    private RadioButton raChoose;
+    private FrameLayout frameQuestion;
+    private LinearLayout trueFalse;
+    private RadioButton rach1;
+    private RadioButton rach2;
+    private RadioButton rach3;
+    private RadioButton rach4;
+    private EditText etch1;
+    private EditText etch2;
+    private EditText etch3;
+    private EditText etch4;
+    private RadioButton rbtrue;
+    private RadioButton rbfalse;
+    private Button buAdd;
+    private Button buSubmit;
+    private View view;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +44,36 @@ public class CreateQuiz extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_quiz, container, false);
+        view = inflater.inflate(R.layout.fragment_create_quiz, container, false);
+        findViews(view);
+
+
+
+
+        return view;
     }
 
+    private void findViews(View view) {
+        tvnameQuize = (EditText) view.findViewById(R.id.tvnameQuize);
+        tvDiscription = (EditText) view.findViewById(R.id.tvDiscription);
+        tvQuestion = (EditText) view.findViewById(R.id.tvQuestion);
+        raObtion = (RadioButton) view.findViewById(R.id.raObtion);
+        raChoose = (RadioButton) view.findViewById(R.id.raChoose);
+        frameQuestion = (FrameLayout) view.findViewById(R.id.frameQuestion);
+        trueFalse = (LinearLayout) view.findViewById(R.id.trueFalse);
+        rach1 = (RadioButton) view.findViewById(R.id.rach1);
+        rach2 = (RadioButton) view.findViewById(R.id.rach2);
+        rach3 = (RadioButton) view.findViewById(R.id.rach3);
+        rach4 = (RadioButton) view.findViewById(R.id.rach4);
+        etch1 = (EditText) view.findViewById(R.id.etch1);
+        etch2 = (EditText) view.findViewById(R.id.etch2);
+        etch3 = (EditText) view.findViewById(R.id.etch3);
+        etch4 = (EditText) view.findViewById(R.id.etch4);
+        rbtrue = (RadioButton) view.findViewById(R.id.rbtrue);
+        rbfalse = (RadioButton) view.findViewById(R.id.rbfalse);
+        buAdd = (Button) view.findViewById(R.id.buAdd);
+        buSubmit = (Button) view.findViewById(R.id.buSubmit);
+
+
+    }
 }
--- app/src/main/java/ps/wwbtraining/teacher_group1/TeacherApi.java
+++ app/src/main/java/ps/wwbtraining/teacher_group1/TeacherApi.java
@@ -5,44 +5,20 @@ import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by Hanan Dawod on 18/10/17.
  */
 
 public interface TeacherApi {
-/*
- /* User user = new User();
-    Boolean b = false;
-    public boolean check(String user_password,String user_email) {
-        teacherApi.checkTeacher(user_password,user_email).enqueue(new Callback<User>() {
-            @Override
-            public void onResponse(Call<User> call, Response<User> response) {
-
-                if(response.isSuccessful()) {
-                    //user.getResult().equals("true");
-                    Toast.makeText(getActivity(), "sucess +  "+user.getResult(), Toast.LENGTH_SHORT).show();
-                    b=true;
-                   // new MainActivity().replaceLoginFragment();
-
-                }
-            }
-
-            @Override
-            public void onFailure(Call<User> call, Throwable t) {
-                Toast.makeText(getActivity(), "Unable to submit post to API.", Toast.LENGTH_SHORT).show();
-
-            }
-
-
-        });
-        return b;
-}*/
-
     @GET("check.php")
     @FormUrlEncoded
     Call<User> checkTeacher(@Field("user_password") String user_password,
                             @Field("user_email") String user_email);
+//@POST("Quiz.php")
+//    Call InsertQuiz(@Field()
+//                    @Field());
 
 
 }
